118df0fc954d056d70b4b9a65dec0a4aa07af178
Mojtaba Nabavi <dev.nabavi@gmail.com>
2021-11-15 16:02:02 +0330

    Adding object extensions

---
+++ Loby.Core/Extensions/ObjectExtensions.cs
@@ -0,0 +1,23 @@
+﻿namespace Loby.Core
+{
+    public static class ObjectExtensions
+    {
+        #region Value
+
+        /// <summary>
+        /// Indicates whether the specified object is null.
+        /// </summary>
+        /// <param name="value">
+        /// The object to test.
+        /// </param>
+        /// <returns>
+        /// true if the object parameter is null; otherwise, false.
+        /// </returns>
+        public static bool IsNull(this object value)
+        {
+            return value == null;
+        }
+
+        #endregion;
+    }
+}
