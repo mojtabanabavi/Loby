4e5a236e61933c8e7967cdd653cfc8bbb09288ec
Mojtaba Nabavi <dev.nabavi@gmail.com>
2021-12-03 13:15:09 +0330

    Add pagination extensions

--- Loby/Extensions/EnumerableExtensions.cs
+++ Loby/Extensions/EnumerableExtensions.cs
@@ -77,6 +77,34 @@ namespace Loby.Extensions
 
         #endregion;
 
+        #region Paging
+
+        /// <summary>
+        /// Paginates a set of elements based on <paramref name="page"/> 
+        /// and <paramref name="pageSize"/>.
+        /// </summary>
+        /// <typeparam name="T">
+        /// Type of sequence elements.
+        /// </typeparam>
+        /// <param name="source">
+        /// A set of elements to be paginated.
+        /// </param>
+        /// <param name="page">
+        /// The page number.
+        /// </param>
+        /// <param name="pageSize">
+        /// Number of elements per page.
+        /// </param>
+        /// <returns>
+        /// A set of elements that are located on the specific page.
+        /// </returns>
+        public static IEnumerable<T> Page<T>(this IEnumerable<T> source, int page, int pageSize)
+        {
+            return source.Skip((page - 1) * pageSize).Take(pageSize);
+        }
+
+        #endregion;
+
         #region Utils
 
         /// <summary>
---
+++ Loby/Extensions/IQueryableExtensions.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq;
+
+namespace Loby.Extensions
+{
+    public static class IQueryableExtensions
+    {
+        #region Paging
+
+        /// <summary>
+        /// Paginates a set of elements based on <paramref name="page"/> 
+        /// and <paramref name="pageSize"/>.
+        /// </summary>
+        /// <typeparam name="T">
+        /// Type of sequence elements.
+        /// </typeparam>
+        /// <param name="source">
+        /// A set of elements to be paginated.
+        /// </param>
+        /// <param name="page">
+        /// The page number.
+        /// </param>
+        /// <param name="pageSize">
+        /// Number of elements per page.
+        /// </param>
+        /// <returns>
+        /// A set of elements that are located on the specific page.
+        /// </returns>
+        public static IQueryable<T> Page<T>(this IQueryable<T> source, int page, int pageSize)
+        {
+            return source.Skip((page - 1) * pageSize).Take(pageSize);
+        }
+
+        #endregion;
+    }
+}
