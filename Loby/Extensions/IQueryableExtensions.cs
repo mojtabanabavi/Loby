c2949074f020fe18da9e2b1417ba0ab65182dcdb
Mojtaba Nabavi <dev.nabavi@gmail.com>
2022-03-24 15:08:12 +0430

    Add order by extension method for IQueryable

--- Loby/Extensions/IQueryableExtensions.cs
+++ Loby/Extensions/IQueryableExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using System.Linq.Expressions;
 
 namespace Loby.Extensions
 {
@@ -50,6 +51,64 @@ namespace Loby.Extensions
             }
 
             return source.Skip((page - 1) * pageSize).Take(pageSize);
+        }
+
+        #endregion;
+
+        #region ordering
+
+        /// <summary>
+        /// Sorts the elements of a sequence according to a expression.
+        /// </summary>
+        /// <typeparam name="T">
+        /// The type of the elements of source.
+        /// </typeparam>
+        /// <param name="source">
+        /// A sequence of values to order.
+        /// </param>
+        /// <param name="expression">
+        /// An expression string that indicate how values should be order by.
+        /// </param>
+        /// <returns>
+        /// A <see cref="IQueryable{T}"/> whose elements are sorted according to the 
+        /// specified <paramref name="expression"/>.
+        /// </returns>
+        /// <exception cref="ArgumentNullException">
+        /// source is null.
+        /// </exception>
+        /// <exception cref="ArgumentNullException">
+        /// expression is null.
+        /// </exception>
+        public static IQueryable<T> OrderBy<T>(this IQueryable<T> source, string expression)
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException("source is null.");
+            }
+
+            if (expression == null)
+            {
+                throw new ArgumentException("expression is null.");
+            }
+
+            var expressionItems = expression.Split(' ');
+
+            var propertyName = expressionItems[0];
+            var orderByMethod = expressionItems.Length == 1 ? "OrderBy" :
+                (expressionItems[1].Equals("DESC", StringComparison.OrdinalIgnoreCase) ? "OrderByDescending" : "OrderBy");
+
+            var parameterExpression = Expression.Parameter(source.ElementType);
+            var memberExpression = Expression.Property(parameterExpression, propertyName);
+
+            var query = Expression.Call(
+                typeof(Queryable),
+                orderByMethod,
+                new Type[] { source.ElementType, memberExpression.Type },
+                source.Expression,
+                Expression.Quote(Expression.Lambda(memberExpression, parameterExpression))
+                );
+
+            return source.Provider.CreateQuery(query) as IQueryable<T>;
         }
 
         #endregion;
